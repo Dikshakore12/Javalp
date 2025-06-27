5d981fe22a02da3eb31dbe9396047df297c1c676
Dikshakore12 <korediksha30@gmail.com>
2025-06-27 17:33:09 +0530

    day6

---
+++ src/Methods/Methods21.java
@@ -0,0 +1,27 @@
+package Methods;
+
+public class Methods21 {
+	public static void main(String[] args) {
+		Methods21 obj = new Methods21(); 
+		obj.Sum(); //calling with object
+	}
+	static void Sum() //definition
+	{
+		System.out.println("Diksha Kore");
+	}
+}
+
+
+//without making an obj
+/*package Methods;
+
+public class methods21 {
+	public static void main(String[] args) {
+		Sum(); //calling
+	}
+	static void Sum() //defition
+	{
+		System.out.println("Yash Bajpai");
+	}
+}
+*/
